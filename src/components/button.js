7dde8a54f01586a584f19f50d7f29b56feeb8375
Naman Bansal <namanbansal@Namans-MacBook-Air.local>
2022-05-15 20:45:15 +0530

    full version

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+
+export const Button = ({ color, text, onClick }) => {
+  return (
+    <button className='btn' style={{backgroundColor: color}} onClick={onClick}>{text}</button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'steelBlue'
+}
